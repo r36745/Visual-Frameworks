41dfc7373596734011ca3dcaa294c110436980c4
Steven Roseman <stevierose@fullsail.edu>
2013-03-02 19:11:41 -0500

    All files have been added. Getting an error in editItem function. Saying property is not defined

---
+++ week4/json.js
@@ -0,0 +1,42 @@
+/*
+ Steven Roseman
+ Visual Frameworks
+ Term 1302
+ Project 4
+ */
+
+var json = {
+    "wishList1" :{
+        "fname": ["First Name:", "Steve"],
+        "lname": ["Last Name:", "Roseman"],
+        "email": ["Email:", "stevierose@fullsail.edu"],
+        "products": ["Group:", "posh"],
+        "interest": ["Interest:", "10"],
+        "color": ["Color:", "blueberry blue"],
+        "fav": ["Your Favorite:", "Yes"],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", "product test"]
+    },
+    "wishList2" :{
+        "fname": ["First Name:", "Kadietra"],
+        "lname": ["Last Name:", "Roseman"],
+        "email": ["Email:", "kadietra.rose@gmail.com"],
+        "products": ["Group:","kitten"],
+        "interest": ["Interest:", "4"],
+        "color": ["Color:", "passion pink"],
+        "fav": ["Your Favorite:", "Yes"],
+        "date": ["Date:", "2013-28-01"],
+        "comments": ["Comments:", "just aweosme"]
+    },
+    "wishList3" :{
+        "fname": ["First Name:", "Kish"],
+        "lname": ["Last Name:", "Ferguson"],
+        "email": ["Email:", "kishk@yahoo.com"],
+        "products": ["Group:", "zany"],
+        "interest": ["Interest:", "8"],
+        "color": ["Color:", "refreshing rasberry"],
+        "fav": ["Your Favorite:", "Yes"],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", "valentines gift"]
+    }
+}
